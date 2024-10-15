2d6066781f9004880cc187fca5c4612b748ea9c1
Adelina <144759048+AdelinaPs@users.noreply.github.com>
2024-10-15 15:59:31 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+// Пшеничникова ПМИ-10
+// Нечетный вариант
+
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
 
     public static void main(String[] args) {
         Main n = new Main();
-
+        
         // 1 блок заданий: методы
         System.out.println("Ответы по 1 блоку заданий:\n");
 
@@ -13,7 +17,7 @@ public class Main {
         System.out.println("1.1 задача:");
         double x = 5.25;
         System.out.println("Входящее число: " + x);
-        System.out.printf("Вывод дробной части: " + "%.2f%n", n.fraction(x)); // только 2 знака после запятой
+        System.out.printf("Вывод дробной части: " + "%.2f%n", n.fraction(x));
 
         // вывод 1.3 задания
         System.out.println("\n1.3 задача:");
@@ -71,7 +75,7 @@ public class Main {
 
         // 3 блок заданий: циклы
         System.out.println("Ответы по 3 блоку заданий:\n");
-
+        
         // вывод 3.1 задания
         System.out.println("\n3.1 задача:");
         System.out.println("Строка от 0 до x чисел");
@@ -156,10 +160,10 @@ public class Main {
 
     // 1.5
     public boolean is2Digits() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scaner = new Scanner(System.in);
         System.out.print("Введите число для проверки на двузначность: ");
-        int x = scan.nextInt();
-        if ((x > 99 || x < 10) && (x < -99 || x > -10)) {
+        int x = scaner.nextInt();
+        if ((x < -99 || x > -10) && (x > 99 || x < 10)) {
             return false;
         }
         return true;
@@ -167,15 +171,14 @@ public class Main {
 
     // 1.7
     public boolean isInRange() {
-        Scanner scan = new Scanner(System.in);
-        int a = 1, b = 5;
-        System.out.println("Левая граница диапазона(а) = " + a);
-        System.out.println("Правая граница диапазона(b) = " + b);
+        Scanner scaner = new Scanner(System.in);
+        int a = 5, b = 1;
+        System.out.println("Левая граница диапазона(а) = " + a + "\nПравая граница диапазона(b) = " + b);
         System.out.print("Входящее число в диапазон = ");
-        int num = scan.nextInt();
+        int num = scaner.nextInt();
         if (a < b && num >= a && num <= b) {
             return true;
-        }else if (a > b && num <= a && num >= b){
+        }else if (a > b && num <= a && num >= b) {
             return true;
         }
         return false;
@@ -183,11 +186,9 @@ public class Main {
 
     // 1.9
     public boolean isEqual() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scaner = new Scanner(System.in);
         int a = -1, b = 3, c = 2;
-        System.out.println("1-е число: " + a);
-        System.out.println("2-е число: " + b);
-        System.out.println("3-е число: " + c);
+        System.out.println("1-е число: " + a + "\n 2-е число: " + b + "\n 3-е число: " + c);
         if (a == b && b == c) {
             return true;
         }
@@ -198,9 +199,9 @@ public class Main {
 
     // 2.1
     public int abs() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scaner = new Scanner(System.in);
         System.out.print("Задаваемое число: ");
-        int x = scan.nextInt();
+        int x = scaner.nextInt();
         if (x >= 0){
             return x;
         }
@@ -209,9 +210,9 @@ public class Main {
 
     // 2.3
     public boolean is35() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scaner = new Scanner(System.in);
         System.out.print("Проверяемое число: ");
-        int x = scan.nextInt();
+        int x = scaner.nextInt();
         // Проверяем, делится ли число на 3
         if (x % 3 == 0) {
             // Если число делится на 3, проверяем, делится ли оно также на 5
@@ -237,11 +238,11 @@ public class Main {
 
     // 2.7
     public int sum2() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scaner = new Scanner(System.in);
         System.out.print("Введите два числа: ");
-        int x = scan.nextInt(), y = scan.nextInt();
+        int x = scaner.nextInt(), y = scaner.nextInt();
         int sum = x + y;
-        if (sum >= 10 && sum <= 19) {
+        if ( sum <= 19 && sum >= 10 ) {
             return 20;
         }else{
             return sum;
@@ -273,40 +274,76 @@ public class Main {
     // решение 3 блока заданий: циклы
     // 3.1
     public String listNums() {
-        StringBuilder str = new StringBuilder();
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число для строки: ");
-        int x = scan.nextInt();
+        StringBuilder stroka = new StringBuilder();
+        Scanner scaner = new Scanner(System.in);
+        boolean isValidInput = false;
+        int x = 0; // Объявляем x здесь, вне блока try-catch
+
+        while (!isValidInput) {
+            try {
+                System.out.print("Введите число для строки: ");
+                x = scaner.nextInt();
+
+                if (x >= 0) {
+                    isValidInput = true;
+                } else {
+                    System.out.println("Пожалуйста, введите положительное число или ноль.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Некорректный ввод. Пожалуйста, введите целое число.");
+                scaner.next(); // Очищаем ошибочный ввод
+            }
+        }
+
         for (int i = 0; i <= x; i++) {
             if (i > 0) {
-                str.append(" ");
+                stroka.append(" ");
             }
-            str.append(i);
+            stroka.append(i);
         }
-        return str.toString();
+        return stroka.toString();
     }
+
 
     // 3.3
     public String chet() {
-        StringBuilder str = new StringBuilder();
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число для строки: ");
-        int x = scan.nextInt();
+        StringBuilder stroka = new StringBuilder();
+        Scanner scaner = new Scanner(System.in);
+
+        // Проверка на ввод неотрицательного числа
+        boolean isValidInput = false;
+        int x = 0;
+        while (!isValidInput) {
+            System.out.print("Введите неотрицательное целое число для строки: ");
+            if (scaner.hasNextInt()) {
+                x = scaner.nextInt();
+                if (x >= 0) {
+                    isValidInput = true;
+                } else {
+                    System.out.println("Пожалуйста, введите неотрицательное число.");
+                }
+            } else {
+                System.out.println("Неверный ввод. Пожалуйста, введите целое число.");
+                scaner.next(); // Удаляем неверный ввод из буфера
+            }
+        }
+
         for (int i = 0; i <= x; i += 2) {
             if (i > 0) {
-                str.append(" ");
+                stroka.append(" ");
             }
-            str.append(i);
+            stroka.append(i);
         }
-        return str.toString();
+        return stroka.toString();
     }
+
 
     // 3.5
     public int numLen() {
-        StringBuilder str = new StringBuilder();
-        Scanner scan = new Scanner(System.in);
+        StringBuilder stroka = new StringBuilder();
+        Scanner scaner = new Scanner(System.in);
         System.out.print("Введите число для возвращения знаков в числе: ");
-        long x = scan.nextInt();
+        long x = scaner.nextInt();
         int count = 0;
         while (x != 0) {
             x = x / 10;
@@ -316,10 +353,29 @@ public class Main {
     }
 
     // 3.7
-    public void square(){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число для квадрата: ");
-        int x = scan.nextInt();
+    public void square() {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isValidInput = false;
+        int x = 0;
+
+        while (!isValidInput) {
+            System.out.print("Введите число для квадрата (не меньше нуля): ");
+
+            if (scanner.hasNextInt()) {
+                x = scanner.nextInt();
+
+                if (x >= 0) {
+                    isValidInput = true;
+                } else {
+                    System.out.println("Ошибка: число должно быть не меньше нуля.");
+                }
+            } else {
+                System.out.println("Ошибка: пожалуйста, введите целое число.");
+                scanner.next(); // Обработка неверного ввода
+            }
+        }
+
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < x; j++) {
                 System.out.print("*");
@@ -328,11 +384,29 @@ public class Main {
         }
     }
 
+
     // 3.9
     public void rightTriangle() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число для треугольника: ");
-        int x = scan.nextInt();
+
+        int x = -1; // Инициализируем x с некорректным значением
+
+        while (x < 0) {
+            if (scanner.hasNextInt()) {
+                x = scanner.nextInt();
+
+                if (x >= 0) {
+                    break;
+                } else {
+                    System.out.println("Пожалуйста, введите неотрицательное число.");
+                }
+            } else {
+                System.out.println("Пожалуйста, введите целое число.");
+                scanner.next(); // Удаляем неверный ввод из буфера
+            }
+        }
+
         for (int i = 1; i <= x; i++) {
             // Вывод пробелов
             for (int j = 0; j < x - i; j++) {
@@ -345,6 +419,8 @@ public class Main {
             System.out.println();
         }
     }
+
+
 
     // решение 4 блока заданий: массивы
     // 4.1
